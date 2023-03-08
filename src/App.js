436cefb506e8d5ad1489c768e03b17d703c2a8b7
user <ujjalroy7862@gmail.com>
2023-03-08 22:23:30 +0600

    added the all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPasword';
+import Offers from './pages/Offers';
+
 const App = () => {
     return (
         <>
-            <h1 className="text-red-400 text-2xl">Hello </h1>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/profile" element={<Profile />} />
+                <Route exact path="/sing-in" element={<SignIn />} />
+                <Route exact path="/sing-up" element={<SignUp />} />
+                <Route exact path="/forgot-password" element={<ForgotPassword />} />
+                <Route exact path="/offers" element={<Offers />} />
+            </Routes>
         </>
     );
 };
