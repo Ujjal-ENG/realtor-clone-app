2f36bf2f834f5eb14a916105198b15088f58d0f8
user <ujjalroy7862@gmail.com>
2023-03-08 23:42:25 +0600

    done the header part

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPasword';
 import Offers from './pages/Offers';
+import Header from './components/Header';
 
 const App = () => {
     return (
         <>
+            <Header />
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/profile" element={<Profile />} />
-                <Route exact path="/sing-in" element={<SignIn />} />
-                <Route exact path="/sing-up" element={<SignUp />} />
+                <Route exact path="/sign-in" element={<SignIn />} />
+                <Route exact path="/sign-up" element={<SignUp />} />
                 <Route exact path="/forgot-password" element={<ForgotPassword />} />
                 <Route exact path="/offers" element={<Offers />} />
                 <Route path="*" element="No Path FOund" />
